9da75a8ae7e711716ac12853c9c0e104f9d82e1c
GM2150313\ydauser01 <rinecottmain@gmail.com>
2026-04-23 15:10:05 +0900

    ソート機能基盤実装

--- Select.cpp
+++ Select.cpp
@@ -1,21 +1,27 @@
 ﻿#include "stdafx.h"
 #include "Select.h"
 Select::Select()
-	: selectIndex (1)
+	: selectIndex (0)
 	, canInput(true)
 	, isPlay(false)
 	, descArea{}
 	, movscale(1)
 	, isAnim(false)
 	, prevPos (0,0)
-	, prevDistance(350)
-	, prevCnt(3)
+	, prevDistance(230)
+	, prevCnt(5)//奇数にすること
 	, animDuration(0.5)
 	, next(Direction::none)
 	, nowPos(0, 0)
 	, targetPos(0,0)
+	, prevSize(200)
+	, descPos(0,0)
+	, descSize(0,0)
+	, movPos(0,0)
+	, movSize(0,0)
 {
 	prevArea.resize(prevCnt + 2);//上の部分と下の部分も入れる.
+	nowGameList = &games;//選択しているゲームリストのポインタを初期化.
 }
 Select::~Select()
 {
@@ -26,16 +32,68 @@ void Select::Initialize() {
 	SelectInit();
 }
 void Select::SelectInit() {
-	prevPos = Point(500, 600);//プレビューエリアの座標代入.
+	prevPos = Vec2(150, 600);//プレビューエリアの座標代入.
+	movPos = Vec2(750, 100);//動画の表示位置代入.
+	movSize = Vec2(1200, 675);//動画の表示サイズ代入.
+	descPos = Vec2(750, 800);//説明文の表示位置代入.
+	descSize = Vec2(1000, 350);//説明文の表示サイズ代入.
 	Load();
 	LoadPrev();
 	SetRectSize();
+}
+void Select::InitGenre() {
+	for (int i = 0; i < games.size(); i++) {
+		if (games[i].genre.isEmpty())continue;//ジャンルが空ならスキップ.
+		bool add = true;
+		for (int n = 0; n < items.size(); n++) {
+			if (games[i].genre == items[n]) {
+				add = false;
+				break;
+			}
+		}
+		if (add) {
+			items << games[i].genre;
+		}
+	}
+	
+}
+void Select::InitYear() {
+	for (int i = 0; i < games.size(); i++) {
+		if (games[i].year== 0)continue;//年度が空ならスキップ.
+		bool add = true;
+		for (int n = 0; n < years.size(); n++) {
+			if (games[i].year == years[n]) {
+				add = false;
+				break;
+			}
+		}
+		if (add) {
+			years << games[i].year;
+			items << Format(U"{}年"_fmt(games[i].year));
+		}
+	}
+	
 }
 void Select::Load() {
 	Print << homeDirectory;
 	const FilePath noImgPath = U"data/image/NoImage.png";
 	noImage = Texture{ noImgPath };//動画等が無かった時の画像読み込み.	
 	LoadGames();//ゲーム読み込み.
+	items << U"All";
+	InitGenre();//ジャンルの初期化.
+	InitYear();//年度の初期化.
+	tab = TabE{ Size{ 160, 50 }, items};
+}
+void Select::LoadSortGames() {
+	int selectedTab = tab.getActiveTabIndex();
+	if (selectedTab <= 0 || selectedTab >= items.size()) {
+		nowGameList = &games;//タブのインデックスが不正または0なら全てのゲームを表示&今使っているゲームリストのポインタを更新.
+	}
+	else {
+		selectedGames = SortGames(games, items[selectedTab]);//タブのインデックスに対応する文字列でソート.
+		nowGameList = &selectedGames;//今使っているゲームリストのポインタを更新.
+	}
+	selectIndex = 0;//選択しているゲームのインデックスを初期化.
 }
 //ゲームのパスがWebページかどうか.
 bool Select::IsURL(StringView path) {
@@ -65,11 +123,13 @@ Array<Game> Select::LoadGames() {
 			tmp.write(U"Game", U"desc", U"ゲームの説明文");
 			tmp.write(U"Game", U"staff", U"ゲーム制作スタッフ");
 			tmp.write(U"Game", U"tools", U"制作に使ったツール");
+			tmp.write(U"Game", U"genre", U"ジャンル");
 			tmp.write(U"Game", U"mouse", U"マウス操作 ; true または false");
 			tmp.write(U"Game", U"keyboard", U"キーボード操作 ; true または false");
 			tmp.write(U"Game", U"gamepad", U"ゲームパッド操作 ; true または false");
 			tmp.write(U"Game", U"priority", U"ランチャーでの表示優先度（大きいほど一覧で先頭に表示） ; 整数値");
 			tmp.write(U"Game", U"path", U"ゲームのexe名");
+			tmp.write(U"Game", U"year", U"制作年度.西暦で書くこと(例：2026)");
 
 			tmp.save(iniPath);
 			System::MessageBoxOK(gameDirectory + U" 内にlauncher_info.iniが見つかりませんでした。\nデフォルト値で作成しました。");
@@ -89,6 +149,7 @@ Array<Game> Select::LoadGames() {
 		game.desc = ini[U"Game.desc"].replaced(U"\\n", U"\n");
 		game.staff = ini[U"Game.staff"];
 		game.tools = ini[U"Game.tools"];
+		game.genre = ini.get<String>(U"Game.genre");
 		game.useMouse = ini.get<bool>(U"Game.mouse");
 		game.useKeyboard = ini.get<bool>(U"Game.keyboard");
 		game.useGamepad = ini.get<bool>(U"Game.gamepad");
@@ -97,9 +158,10 @@ Array<Game> Select::LoadGames() {
 		const String path = game.path = ini[U"Game.path"];
 		game.isWebApp = IsURL(path);
 		game.path = (game.isWebApp ? path : (gameDirectory + path));
-
+		game.year = ParseOr<int32>(ini[U"Game.year"], 0);//文字列の変換に失敗したら0を入れる.
 		// ゲームのリストに追加
 		games << game;
+		nowGameList = &games;
 	}
 
 	// プライオリティに基づいてゲームをソート
@@ -129,7 +191,7 @@ void Select::Update() {
 			} 
 		}
 	}
-	if (games.empty()) return;
+	if ((*nowGameList).empty()) return;
 
 	if (!canInput) {
 		//もし最後の入力から0.5秒経っていたらフラグON.
@@ -147,6 +209,16 @@ void Select::Update() {
 		Anim(Direction::down, 0.5);
 		next = Direction::down;
 	}
+	if (KeyLeft.down())
+	{
+		tab.advance(-1);
+		LoadSortGames();
+	}
+	else if (KeyRight.down())
+	{
+		tab.advance(+1);
+		LoadSortGames();
+	}
 	if (isAnim) {
 		double t = Min(t_prev.sF(), animDuration) / animDuration;
 		double e = EaseOutExpo(t);
@@ -163,12 +235,14 @@ void Select::Update() {
 	}
 }
 void Select::Draw() {
+	tab.draw(Vec2{ 300, 40 }, fontMgr.GetFont(), TabColor, TabOutlineColor);
 	DrawPrev();//プレビューを表示.
 	descArea.rounded(40, 0, 40, 0).draw();
-	fontMgr.GetFont()(games[selectIndex].desc).draw(descArea.stretched(-80, -30, -20, -30), Palette::Black);
+	fontMgr.GetFont()((*nowGameList)[selectIndex].desc).draw(descArea.stretched(-80, -30, -20, -30), Palette::Black);
+	//Print << items;
 }
 void Select::LoadPrev() {
-	Print << games.size();
+	Print << (*nowGameList).size();
 	if (audio) {
 		audio.stop();
 		audio = Audio();
@@ -178,17 +252,17 @@ void Select::LoadPrev() {
 
 	//プレビューエリアテクスチャー読み込み.
 	for (int i = 0; i < prevArea.size(); i++) {
-		int32 index = (games.size() + selectIndex + (i - 2)) % games.size();
-		if (!games[index].texture) {
+		int32 index = (int32)((*nowGameList).size() + selectIndex + (i - 2)) % (*nowGameList).size();
+		if (!(*nowGameList)[index].texture) {
 			prevArea[i] = noImage;
 		}
 		else {
-			prevArea[i] = games[index].texture;
+			prevArea[i] = (*nowGameList)[index].texture;
 		}
 	}
 
-	mov = games[selectIndex].mov;						//動画.
-	audio = games[selectIndex].movAudio;			//音声.
+	mov = (*nowGameList)[selectIndex].mov;						//動画.
+	audio = (*nowGameList)[selectIndex].movAudio;			//音声.
 	//音声または動画が見つからなかったら抜ける.
 
 	if (!mov || !audio) {
@@ -197,9 +271,12 @@ void Select::LoadPrev() {
 	audio.play();//音声再生.
 }
 
+
 void Select::DrawPrev() {
+	int center = (int)prevArea.size() / 2;
 	for (int i = 0; i < prevArea.size(); i++) {
-		prevArea[i].fitted(300 * SCALE, 300 * SCALE).rounded(40).drawAt(prevPos.x * SCALE, (prevPos.y + prevDistance * (i - 2)) * SCALE);
+		int offset = i - center;
+		prevArea[i].fitted(prevSize * SCALE, prevSize * SCALE).rounded(40).drawAt(prevPos.x * SCALE, (prevPos.y + offset * prevDistance ) * SCALE);
 	}
 	PlayMov();//動画を再生.
 }
@@ -212,10 +289,10 @@ void Select::Anim(Direction dir, double sec) {
 		t_prev.restart();
 		animDuration = sec;
 		if (dir == Direction::up) {
-			targetPos = { prevPos.x, prevPos.y + 350 };
+			targetPos = { prevPos.x, prevPos.y + prevDistance };
 		}
 		else {
-			targetPos = { prevPos.x, prevPos.y - 350 };
+			targetPos = { prevPos.x, prevPos.y - prevDistance };
 		}
 	}
 }
@@ -227,12 +304,12 @@ void Select::UpdateIndex(Direction dir) {
 	case Direction::up:
 		selectIndex--;
 		if (selectIndex < 0) {
-			selectIndex = (int32)games.size() - 1;
+			selectIndex = (int32)(*nowGameList).size() - 1;
 		}
 		break;
 	case Direction::down:
 		selectIndex++;
-		if (selectIndex >= (int32)games.size()) {
+		if (selectIndex >= (int32)(*nowGameList).size()) {
 			selectIndex = 0;
 		}
 		break;
@@ -244,7 +321,7 @@ void Select::UpdateIndex(Direction dir) {
 void Select::PlayMov() {
 	if (isPlay) return;
 	if (!mov) {
-		noImage.scaled(0.6).draw(750 * SCALE, 50 * SCALE);
+		noImage.fitted(movSize.x, movSize.y).draw(movPos.x * SCALE, movPos.y * SCALE);
 		return;
 	}
 	ClearPrint();
@@ -259,18 +336,41 @@ void Select::PlayMov() {
 	//動画の再生位置と音声の再生位置の差が0.1秒以上ある場合
 	if (0.1 < AbsDiff(audioTime, videoTime)) {
 		audio.seekTime(videoTime);
-	}
 
+	}
 	//動画の時間を進める
 	mov.advance();
 
-	mov.fitted(1152,720).draw(750 * SCALE,50 * SCALE);
+	mov.fitted(movSize.x,movSize.y).draw(movPos.x * SCALE,movPos.y * SCALE);
 }
 void Select::SetRectSize() {
-	descArea = RectF{ 750 * SCALE,800 * SCALE,1150 * SCALE,350* SCALE };
+	descArea = RectF{ descPos.x * SCALE,descPos.y * SCALE,descSize.x * SCALE,descSize.y* SCALE };
 }
 void Select::PlayGame() {
-	process = ChildProcess{ games[selectIndex].path };
+	process = ChildProcess{ (*nowGameList)[selectIndex].path };
 	audio.stop();
 	mov.reset();
+}
+
+Array<Game> Select::SortGames(Array<Game> gameList,String moji) {
+	Array<Game> result;
+	//年度での検索を試みる(最初に数字があるかどうかしか見てない).
+	try {
+		String yearStr = moji.remove(U"年");
+		int32 num = Parse<int32>(yearStr);
+		for (int i = 0; i < games.size(); i++) {
+			if (games[i].year == num) {
+				result << games[i];
+			}
+		}
+	}
+	//数字がなかったらジャンル検索する.
+	catch(const ParseError& error){
+		for (int i = 0; i < games.size(); i++) {
+			if (games[i].genre.contains(moji)) {
+				result << games[i];
+			}
+		}
+	}
+	return result;
 }
--- Select.h
+++ Select.h
@@ -2,6 +2,7 @@
 #include "BaseScene.h"
 #include "Application.h"
 #include "FontMgr.h"
+#include "Tab.h"
 //ゲーム情報入れる構造体.
 struct Game {
 	String title;							//ゲームのタイトル.
@@ -13,6 +14,8 @@ struct Game {
 	String desc;							//ゲームの説明文.
 	String staff;						//ゲームの開発スタッフ.
 	String tools;						//ゲームの開発ツール.
+	String genre;						//ゲームのジャンル.
+	int32 year;							//ゲーム制作年度.2026や2025など.
 	bool useMouse = false;		//マウスを使用するか.
 	bool useKeyboard = false;	//キーボードを使用するか.
 	bool useGamepad = false;	//ゲームパッドを使用するか.
@@ -30,30 +33,49 @@ private:
 		right,
 		none
 	};
+	
 	FontMgr& fontMgr = FontMgr::GetInstance();
 	const FilePath homeDirectory = FileSystem::CurrentDirectory();//ホームディレクトリ格納.
+# pragma region プレビューエリア系変数
 	Texture noImage;
 	Array<Texture> prevArea;
 	int32 prevCnt;//プレビューエリアの数.
-	Vec2 prevPos;//プレビューアイコンの一番上の座標.
+	Vec2 prevPos;//プレビューアイコンの真ん中の座標.
+	Vec2 movPos;//動画の表示位置.
+	Vec2 movSize;//動画の表示サイズ.
+	Vec2 descPos;//説明文の表示位置.
+	Vec2 descSize;//説明文の表示サイズ.
 	int prevDistance;//プレビューアイコンの間の距離.
+	int32 prevSize;//プレビューアイコンのサイズ.
 	Stopwatch t_prev;//プレビューアイコン用タイマー.
 	Vec2 nowPos;//今の位置記録.
+# pragma endregion
+#pragma region セレクトアニメーション
 	Vec2 targetPos;//アニメーションの目標位置記録.
 	Direction next;//セレクトアニメーション方向保持.
 	double animDuration;//セレクトアニメーション遷移時間.
-
+#pragma endregion
+#pragma region タブ関連
+	const Font font{ FontMethod::MSDF, 48, Typeface::Heavy };
+	Array<String> items = {};
+	Array<int32> years = {};
+	ColorF TabColor{ 0.2, 0.5, 0.9 };
+	ColorF TabOutlineColor{ 0.5 };
+	ColorF ContentColor{ 0.5 };
+	TabE tab{ Size{ 160, 50 }, items };
+#pragma endregion
+	
 	int32 selectIndex = 0;	//選択しているタイルのインデックス.
-
-
-	RectF descArea;
+	RectF descArea;				//説明エリア.
 
 	Array<Game> games;		//ゲームのリスト.
+	Array<Game> selectedGames;	//優先度でソートしたゲームのリスト.
+	Array<Game>* nowGameList;//今使っているゲームリストのポインタ.
 	VideoTexture mov;	//動画オブジェクト.
 	Audio audio;	//動画再生時のAudioオブジェクト.
 	double movscale;	//動画のサイズ.
 	Stopwatch t_input;//入力制限用のストップウォッチ.
-	bool canInput;
+	bool canInput;//入力可能か.
 	bool isPlay;//ゲーム中か.
 	bool isAnim;//アニメーション.
 
@@ -62,6 +84,8 @@ public:
 	Select();
 	~Select();
 	void Initialize() ;
+	void InitGenre();	//ジャンル情報からジャンルリストを作成.
+	void InitYear();//制作年度情報から年度リストを作成.
 	void SelectInit();//初期化の中身ここに書く.
 	void Finalize(){};
 	void Update();
@@ -72,10 +96,13 @@ public:
 
 	void SetRectSize();//UIサイズ等変更する.
 	void LoadPrev();	//現在選択中のインデックスから動画と音声をロード？.
+	
 	void PlayMov();
 	void PlayGame();//ゲームを開始.
+	Array<Game> SortGames(Array<Game> gameList,String moji);//ゲームの検索結果を返す(Array<Game>型).
 
 	void Load();
+	void LoadSortGames();
 	
 	Array<Game>LoadGames();
 	bool IsURL(StringView path);
